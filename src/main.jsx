a34e72bac4223d10f55bf9d25041500693c7946d
hasibmuhammad <hasibmu99@gmail.com>
2023-11-18 14:00:19 +0600

    Observer added

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Root from "./Root/Root.jsx";
 import NotFound from "./components/NotFound/NotFound.jsx";
 import Register from "./components/Register/Register.jsx";
 import Login from "./components/Login/Login.jsx";
+import AuthProvider from "./providers/AuthProvider.jsx";
 
 const router = createBrowserRouter([
   {
@@ -31,6 +32,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
